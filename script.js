7cfcc2f1b2a25580374e3665623cf07b10ce433f
lina-alrehaili <lina.f.alrehaili@gmail.com>
2022-06-30 01:20:35 +0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('normal');
 
 btn.addEventListener('click', function onClick(event) {
-    const boxes = document.querySelectorAll('#box');
+    const boxes = document.querySelectorAll('.box');
     boxes.forEach(box => {
       box.style.fontSize = '32px';
     });
@@ -10,7 +10,7 @@ btn.addEventListener('click', function onClick(event) {
 const btn2 = document.getElementById('large');
 
 btn2.addEventListener('click', function onClick(event) {
-  const boxes = document.querySelectorAll('#box');
+  const boxes = document.querySelectorAll('.box');
   boxes.forEach(box => {
     box.style.fontSize = '40px';
   });
